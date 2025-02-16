967c06a798cb40eb45a1c05e9f3b6a803bf78fce
raffaem <raffaele.mancuso4@unibo.it>
2025-02-16 12:20:15 +0000

    Deploying to gh-pages from @ raffaem/raffaem.github.io@18a6c01d69590e406d800650d8c25a093a7e01f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,18 +51,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "post-a-post-with-image-galleries",
-      
-        title: "a post with image galleries",
-      
-      description: "this is what included image galleries could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/photo-gallery/";
-        
-      },
-    },{id: "post-ois24-paper-acceptance",
+        },{id: "post-ois24-paper-acceptance",
       
         title: "OIS24 Paper acceptance",
       
